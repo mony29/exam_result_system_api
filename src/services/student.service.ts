0adebbb73d839113db87165c57a3e8066dcc73e3
mony <mony170.infinity@gmail.com>
2026-05-05 17:22:28 +0700

    move data folder outside src directory

--- src/services/student.service.ts
+++ src/services/student.service.ts
@@ -1,17 +1,12 @@
 import path from "path";
 import fs from "fs";
 import type { Student } from "../types/student.js";
-import { fileURLToPath } from "url";
-
-// Re-create __dirname in ESM
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 class StudentService {
   private studentsMap: Map<string, Student> = new Map();
 
   loadData() {
-    const filePath = path.join(__dirname, "../data/students_100k.json");
+    const filePath = path.join(process.cwd(), "/data/students_100k.json");
     const raw = fs.readFileSync(filePath, "utf-8");
     const students: Student[] = JSON.parse(raw);
 
