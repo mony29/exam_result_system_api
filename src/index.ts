49dbe7b1c7348572c8ec1e73de90eb829c3d95c4
mony <mony170.infinity@gmail.com>
2026-05-05 16:22:09 +0700

    Enable CORS for frontend to integrate

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+import 'dotenv/config';
 import express from 'express';
 import { studentService } from './services/student.service.js';
 import resultRoute from './routes/result.route.js';
 import { apiLimiter } from './middlewares/rateLimiter.js';
+import cors from 'cors';
 
 const BASE_URL = "/api";
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT ?? 3001;
+const FRONTEND_URL = process.env.FRONTEND_URL ?? "http://localhost:3000";
+
+// Enable CORS
+app.use(
+  cors({
+    origin: FRONTEND_URL,
+  })
+)
 
 // Load data before starting the server
 studentService.loadData();
@@ -15,6 +25,6 @@ app.set("trust proxy", 1); // ADD Trust proxy for faking IP
 app.use(BASE_URL, apiLimiter);
 app.use(BASE_URL, resultRoute);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
 })
